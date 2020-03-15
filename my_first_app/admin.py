e72a9a55db53951b06d227b1268678d8378380e2
dharamj-cloud <dharamjakhar12@outlook.com>
2020-03-15 23:38:06 +0000

    make models

--- my_first_app/admin.py
+++ my_first_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Login,Class,Student
 
 # Register your models here.
+
+admin.site.register(Login)
+admin.site.register(Class)
+admin.site.register(Student)
--- my_first_app/models.py
+++ my_first_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Login(models.Model):
+       userName=models.CharField(max_length=30)
+       email=models .CharField(max_length=30)
+       password=models.CharField(max_length=30)
+       
+       def __str__(self):
+            return self.userName
+       
+       
+class Class(models.Model):
+        session=models.CharField(max_length=30)
+        
+        def __str__(self):
+            return self.session
+            
+class Student(models.Model):
+        name =models.CharField(max_length=30, unique=False)
+        session=models.ForeignKey("Class",on_delete=models.PROTECT)
+        
+        def __str__(self):
+            return self.name,self.session
